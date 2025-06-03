18d34c12e7144c2bef36bb2817e0983d0ae52d2d
Aditya Kamat <adkamat@uwaterloo.ca>
2025-06-03 13:08:09 -0400

    Add solution for two-sum

---
+++ leetcode/two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+
+        for i in range(len(nums)):
+            numMap[nums[i]] = i
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
